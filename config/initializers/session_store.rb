e86c9cd2bacaf8f541aaeea781b7cc0feb5b95bc
Kojiro <eveve0418@gmail.com>
2018-09-24 05:40:53 +0000

    herokuで動くようにする

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -5,7 +5,7 @@
 
 # Redis
 Rails.application.config.session_store :redis_store,
-    servers: ["redis://localhost:6379/0/session"],
+    servers: ENV['REDIS_URL'] || ["redis://localhost:6379/0/session"],
     expire_after: 90.minutes,
     key: "_#{Rails.application.class.parent_name.downcase}_session",
     threadsafe: false
